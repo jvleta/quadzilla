9fa23222749d9b14ad486d3a37ceb1b72e1679ba
Jimmy Leta <jimmy.leta@gmail.com>
2024-01-25 20:21:09 -0500

    Implemented first pass at composite trapezoid rule

--- main.cpp
+++ main.cpp
@@ -1,23 +1,41 @@
 #include <iostream>
+#include <ratio>
 
 #include "quadzilla.h"
 
 #include "gtest/gtest.h"
 
-TEST(BasicTests, Square) {
+TEST(TrapezoidTests, Square) {
   constexpr double side_length = 5.0;
   constexpr double expected_area = 25.0;
   const auto integrand = [](double x) { return side_length; };
-  EXPECT_EQ(expected_area, quad(integrand, 0.0, side_length));
+  EXPECT_EQ(expected_area, trapezoid(integrand, 0.0, side_length));
 }
 
-TEST(BasicTests, Triangle1) {
+TEST(TrapezoidTests, SawTooth) {
   const auto integrand = [](double x) {
     constexpr double base = 1.0;
     constexpr double height = 1.0;
     return height / base * x;
   };
-  ASSERT_EQ(0.5, quad(integrand, 0.0, 1.0));
+  ASSERT_EQ(0.5, trapezoid(integrand, 0.0, 1.0));
+}
+
+TEST(CompositeTrapezoidTests, Parabola) {
+  const auto x_squared = [](double x) { return x * x; };
+  const double expected = 2.0 / 3.0;
+  const auto check = [&](int num_intervals, double tolerance) {
+    const double estimated =
+        composite_trapezoid(x_squared, -1.0, 1.0, num_intervals);
+    const double relative_error = std::abs((expected - estimated) / expected);
+    ASSERT_LE(relative_error, tolerance);
+  };
+  check(1000, 1e-2);
+  check(10000, 1e-3);
+  check(100000, 1e-4);
+  check(1000000, 1e-5);
+  check(10000000, 1e-6);
+  check(100000000, 1e-7);
 }
 
 int main(int argc, char **argv) {
--- quadzilla.h
+++ quadzilla.h
@@ -3,10 +3,28 @@
 
 #include <functional>
 
-double quad(std::function<double(double)> func, double a, double b){
-    double height = 0.5 * (func(b) + func(a));
-    double base = b - a;
-    return base * height;
+double trapezoid(std::function<double(double)> integrand, double left_endpoint,
+                 double right_endpoint) {
+  const double length1 = integrand(right_endpoint);
+  const double length2 = integrand(left_endpoint);
+  const double average_length = 0.5 * (length1 + length2);
+  const double width = right_endpoint - left_endpoint;
+  return average_length * width;
+}
+
+double composite_trapezoid(std::function<double(double)> integrand,
+                           double left_endpoint, double right_endpoint,
+                           int num_intervals) {
+  double result = 0.0;
+  const double dx =
+      (right_endpoint - left_endpoint) / static_cast<double>(num_intervals - 1);
+  
+  for (int i = 0; i < num_intervals; ++i) {
+    const double a = left_endpoint + i * dx;
+    const double b = a + dx;
+    result += trapezoid(integrand, a, b);
+  }
+  return result;
 }
 
 #endif
